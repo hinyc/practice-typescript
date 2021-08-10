fb34cedad8a604acb388374c5a734bfd432fcf3f
hinyc <dlsduf@gmail.com>
2021-08-10 22:16:50 +0900

    수정

--- interface.ts
+++ interface.ts
@@ -1,8 +1,25 @@
-function hello1(person: { name: string; age: number }): void {
+// function hello1(person: { name: string; age: number }): void {
+//   console.log(`안녕하세요! ${person.name} 입니다.`);
+// }
+
+// const p1: { name: string; age: number } = {
+//   name: "hinyc",
+//   age: 31,
+// };
+
+// hello1(p1);
+
+
+interface person1 { 
+  name: string; 
+  age: number
+ }
+
+function hello1(person: person1 ): void{
   console.log(`안녕하세요! ${person.name} 입니다.`);
 }
 
-const p1: { name: string; age: number } = {
+const p1: person1 = {
   name: "hinyc",
   age: 31,
 };
